5d8628f35b9d1f87aa3812868cba7f71b536401a
aadhargupta9 <aadhargupta9@gmail.com>
2019-09-24 15:55:40 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { EmployeeService } from './employee.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private emp: EmployeeService){
+
+  }
   title = 'servicesdemo';
+  emp_id = null;
+  emp_name = null;
+  emp_dept =null;
+  myEmployee = {
+    id:1,
+    name:'',
+    dept:''
+  }
+  addEmployee()
+  {
+    this.myEmployee.id=this.emp_id;
+    this.myEmployee.name=this.emp_name;
+    this.myEmployee.dept=this.emp_dept;
+    this.emp.setEmployee(this.myEmployee);
+  }
 }
